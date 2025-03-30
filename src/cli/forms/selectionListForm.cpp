164247bc4a9e689c844d4eee7e9ae2a31b7b4de8
Matvey <matveigavrilov23@gmail.com>
2025-03-30 21:09:30 +0300

    Showing selection form fix

--- src/cli/forms/selectionListForm.cpp
+++ src/cli/forms/selectionListForm.cpp
@@ -7,14 +7,16 @@ selectionListForm::selectionListForm(size_t x, size_t y, size_t width, size_t he
 : form(x, y, width, height)
 , items_(items)
 , horizontal_(horizontal)
+, offset_(0)
 { }
 
 void selectionListForm::show(window& wnd)
 {
 	size_t x = x_, y = y_;
 	size_t edgeX_ = x_ + width_, edgeY_ = y_ + height_;
-	auto index = 0;
-	while (y != edgeY_ && x != edgeX_ && index < items_.size())
+	size_t index = offset_;
+
+	while (y < edgeY_ && x < edgeX_ && index < items_.size())
 	{
 		const auto& item = items_[index];
 		auto nextY = y + linesNeeded(item);
@@ -29,6 +31,8 @@ void selectionListForm::show(window& wnd)
 		if (horizontal_)
 		{
 			x += item.size() + 1;
+			if (x >= edgeX_)
+				break;
 		}
 		else
 		{
@@ -41,6 +45,8 @@ void selectionListForm::show(window& wnd)
 void selectionListForm::addItem(const item_type& item)
 {
 	items_.push_back(item);
+	// Adjust offset if needed when adding new items
+	adjustOffset();
 }
 
 void selectionListForm::removeItem(const item_type& item) { }
@@ -73,6 +79,7 @@ void selectionListForm::switchUp()
 	{
 		selectedIndex_ -= 1;
 	}
+	adjustOffset();
 }
 
 void selectionListForm::switchDown()
@@ -89,6 +96,7 @@ void selectionListForm::switchDown()
 	{
 		selectedIndex_ += 1;
 	}
+	adjustOffset();
 }
 
 void selectionListForm::showSelected()
@@ -106,6 +114,7 @@ void selectionListForm::clear()
 	items_.clear();
 	selectedIndex_ = 0;
 	showSelected_ = false;
+	offset_ = 0;
 }
 
 bool selectionListForm::empty()
@@ -124,6 +133,9 @@ void selectionListForm::showItem(size_t x, size_t y, const std::string& str, win
 	auto tmp = x;
 	for (auto ch : str)
 	{
+		if (x >= edgeX_ || y >= edgeY_)
+			break;
+
 		wnd.setChar(x, y, ch);
 		wnd.setAttribute(x++, y, selected && showSelected_ ? window::HIGHLIGHT_COLOR : window::DEFAULT_COLOR);
 		if (x >= edgeX_)
@@ -136,10 +148,61 @@ void selectionListForm::showItem(size_t x, size_t y, const std::string& str, win
 			x = tmp;
 		}
 	}
-	while (x < edgeX_)
+	while (x < edgeX_ && y < edgeY_)
 	{
 		wnd.setChar(x, y, ' ');
 		wnd.setAttribute(x++, y, window::DEFAULT_COLOR);
 	}
 }
+
+void selectionListForm::adjustOffset()
+{
+	if (items_.empty())
+		return;
+
+	if (horizontal_)
+	{
+		offset_ = 0;
+		return;
+	}
+
+	size_t visibleItems = 0;
+	size_t y = y_;
+	size_t edgeY_ = y_ + height_;
+
+	for (size_t i = offset_; i < items_.size(); ++i)
+	{
+		size_t needed = linesNeeded(items_[i]);
+		if (y + needed > edgeY_)
+			break;
+		y += needed;
+		visibleItems++;
+	}
+
+	if (selectedIndex_ < offset_)
+	{
+		offset_ = selectedIndex_;
+	}
+	else if (selectedIndex_ >= offset_ + visibleItems)
+	{
+		size_t newOffset = selectedIndex_;
+		y = y_;
+
+		while (newOffset > 0)
+		{
+			size_t needed = linesNeeded(items_[newOffset]);
+			if (y + needed > edgeY_)
+				break;
+			y += needed;
+			newOffset--;
+		}
+
+		offset_ = newOffset;
+		if (y + linesNeeded(items_[selectedIndex_]) > edgeY_)
+		{
+			offset_++;
+		}
+	}
+}
+
 } // namespace tk
